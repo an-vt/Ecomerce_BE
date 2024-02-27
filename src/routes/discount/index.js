47d4f09e8a377509e29e26e771e4638b36965dbc
anvt <truonganvu2000@gmail.com>
2024-02-28 03:12:55 +0700

    Section20: Add controller & route

--- src/routes/discount/index.js
+++ src/routes/discount/index.js
@@ -6,21 +6,16 @@ const { asyncHandler } = require("../../helpers/asyncHandler");
 const { authenticationV2 } = require("../../auth/authUtils");
 const router = express.Router();
 
-// search in here because user not authenticate still can search
-// router.get("/search/:keySearch", asyncHandler(discountController.getListSearchProduct));
-// router.get("", asyncHandler(discountController.findAllProducts));
-// router.get("/:product_id", asyncHandler(discountController.findProduct));
+router.get("/all", asyncHandler(discountController.getAllDiscountCodeByShopId));
+router.get(
+  "/product/:discountCode",
+  asyncHandler(discountController.getAllProductByDiscountCode)
+);
 
 // authentication before logout
 router.use(authenticationV2);
 
 router.post("", asyncHandler(discountController.createDiscount));
 router.patch("/:discountId", asyncHandler(discountController.updateDiscount));
-// router.post("/publish/:id", asyncHandler(discountController.publishProductByShop));
-// router.post("/unpublish/:id", asyncHandler(discountController.unpublishProductByShop));
-
-// Query
-// router.get("/drafts/all", asyncHandler(discountController.getAllDraftsForShop));
-// router.get("/published/all", asyncHandler(discountController.getAllPublishForShop));
 
 module.exports = router;
--- src/services/apiKey.service.js
+++ src/services/apiKey.service.js
@@ -4,6 +4,10 @@ const apiKeyModel = require("../models/apiKey.model");
 const crypto = require("crypto");
 
 const findById = async (key) => {
+  // await apiKeyModel.create({
+  //   key: crypto.randomBytes(64).toString("hex"),
+  //   permissions: ["0000"],
+  // });
   const objKey = await apiKeyModel.findOne({ key, status: true }).lean();
   return objKey;
 };
