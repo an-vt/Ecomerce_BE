4bed3a9d561aae7615b65df26825cd314ec43806
Vu An <truonganvu2000@gmail.com>
2024-05-24 11:10:34 +0700

    Public link image in s3

--- src/services/aws.upload.service.js
+++ src/services/aws.upload.service.js
@@ -1,21 +1,39 @@
 "use strict";
-const { s3, PutObjectCommand } = require("../configs/s3.config");
+const {
+  s3,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("../configs/s3.config");
+const { BadRequestError } = require("../core/error.response");
+const crypto = require("node:crypto");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const randomImageName = () => crypto.randomBytes(16).toString("hex");
 
 // 1. upload from url image
 const uploadImageFromLocalS3 = async ({ file }) => {
   try {
-    const randomImageName = () => crypto.randomBytes(16).toString("hex");
+    const imageName = randomImageName();
     const command = new PutObjectCommand({
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: randomImageName() || "unknown",
+      Key: imageName || "unknown",
       Body: file.buffer,
       ContentType: "image/jpeg",
     });
 
-    const result = await s3.send(command);
-    console.log("result", result);
+    await s3.send(command);
+
+    const singeUrl = new GetObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: imageName,
+    });
+
+    const url = await getSignedUrl(s3, singeUrl, { expiresIn: 3600 });
+
+    return url;
   } catch (error) {
     console.log("Error upload image from local s3", error);
+    throw new BadRequestError("Error upload image from local s3");
   }
 };
 
