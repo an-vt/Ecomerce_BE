5ee465c6eb646193953c4a848d19f65f1adce281
Vu An <truonganvu2000@gmail.com>
2024-05-31 16:02:05 +0700

    Section77: create model for handle role system for user

---
+++ src/models/resource.model.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const { Schema, model } = require("mongoose"); // Erase if already required
+
+const DOCUMENT_NAME = "Resource";
+const COLLECTION_NAME = "Resources";
+
+// Declare the Schema of the Mongo model
+var resourceSchema = new Schema(
+  {
+    src_name: { type: Number, required: true }, //profile
+    src_slug: { type: String, required: true }, // 00001
+    src_description: { type: String, default: "" },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, resourceSchema);
---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { Schema, model } = require("mongoose"); // Erase if already required
+
+const DOCUMENT_NAME = "User";
+const COLLECTION_NAME = "Users";
+
+// Declare the Schema of the Mongo model
+var userSchema = new Schema(
+  {
+    usr_id: { type: Number, required: true },
+    usr_slug: { type: String, required: true },
+    usr_name: { type: String, default: "" },
+    usr_password: { type: String, default: "" },
+    usr_salt: { type: String, default: "" },
+    usr_password: { type: String, default: "" },
+    usr_email: { type: String, required: true },
+    usr_phone: { type: String, default: "" },
+    usr_sex: { type: String, default: "" },
+    usr_avatar: { type: String, default: "" },
+    usr_date_of_birth: { type: Date, default: null },
+    usr_role: { type: Schema.Types.ObjectId, reg: "Role" },
+    usr_status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "active", "block"],
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, userSchema);
